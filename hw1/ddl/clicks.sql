2d3e3cbe90be5894b989ce8cee84d49b52fc08d0
nikitac25 <72084235+nikitac25@users.noreply.github.com>
2025-06-04 05:09:21 +0200

    Update clicks.sql

--- hw1/ddl/clicks.sql
+++ hw1/ddl/clicks.sql
@@ -1,1 +1,12 @@
-
+/*
+  Table stores click events associated with impressions.
+  Clicks are isolated to simplify metrics calculations such as CTR.
+  click_id is the PRIMARY KEY.
+  impression_id is a FOREIGN KEY referencing impressions(id), ensuring only valid impressions are clicked.
+ */
+CREATE TABLE clicks (
+    click_id INT PRIMARY KEY,
+    impression_id INT NOT NULL,
+    click_timestamp DATETIME,
+    FOREIGN KEY (impression_id) REFERENCES impressions(id)
+);
