23f693791fecb9924634caebb6d365e30f973f3e
nikitac25 <72084235+nikitac25@users.noreply.github.com>
2025-06-17 10:22:20 +0200

    Create insert_data.sh

---
+++ hw2/insert_data.sh
@@ -0,0 +1,12 @@
+docker run -it --rm \
+  -v "$(pwd)/python:/app" \
+  -v "$(pwd)/datasources:/data" \
+  -w /app \
+  python:3.11 \
+  bash -c "pip install pandas mysql-connector-python && \
+           python locations.py /data/users.csv /data/campaigns.csv /data/ad_events.csv && \
+           python advertisers.py /data/campaigns.csv && \
+           python users.py /data/users.csv && \
+           python campaigns.py /data/campaigns.csv && \
+           python impressions.py /data/ad_events.csv && \
+           python clicks.py /data/ad_events.csv"
