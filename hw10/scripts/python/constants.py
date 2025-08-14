8b6ca5faa55085328196ccd539fc2b68b4e2897a
nikitac25 <n.d.chikishev@gmail.com>
2025-08-14 17:29:37 +0200

    provided storage for constants

---
+++ hw10/scripts/python/constants.py
@@ -0,0 +1,10 @@
+import os
+BOOTSTRAP_SERVERS ='kafka-server:9092'
+CASSANDRA_HOST = 'cassandra'
+CASSANDRA_KEYSPACE = 'wikipedia'
+CASSANDRA_PORT = '9042'
+CASSANDRA_TABLE = 'pages'
+KAFKA_BOOTSTRAP_SERVERS = 'kafka:9092'
+KAFKA_TOPIC = 'pages'
+WIKIPEDIA_ENDPOINT = 'https://stream.wikimedia.org/v2/stream/page-create'
+
