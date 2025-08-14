32847f1e19193169f9fb912b7ac23baa9211fd74
nikitac25 <n.d.chikishev@gmail.com>
2025-08-15 00:37:59 +0200

    created a project runner for demonstration purposes

---
+++ hw10/runner.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+if ! command -v dos2unix >/dev/null 2>&1; then
+  sudo apt-get update && sudo apt-get install -y dos2unix
+fi
+
+docker compose up -d --wait
+timeout 120 docker compose logs -f producer
+docker compose down
