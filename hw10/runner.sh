3af590903c39653c8ccc6f3f2ded3d13e39c4926
nikitac25 <n.d.chikishev@gmail.com>
2025-08-15 00:59:16 +0200

    updated runner

--- hw10/runner.sh
+++ hw10/runner.sh
@@ -5,6 +5,6 @@ if ! command -v dos2unix >/dev/null 2>&1; then
   sudo apt-get update && sudo apt-get install -y dos2unix
 fi
 
-docker compose up -d --wait
-timeout 120 docker compose logs -f producer
-docker compose down
+docker-compose up -d --wait
+timeout 120 docker-compose logs -f producer
+docker-compose down
