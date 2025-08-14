bbf430dea8d8ee5871707f09703bf07380530f0b
nikitac25 <n.d.chikishev@gmail.com>
2025-08-14 17:29:21 +0200

    provided script for cassandra table creator

---
+++ hw10/scripts/python/cassandra_table_creator.py
@@ -0,0 +1,30 @@
+from cassandra.cluster import Cluster
+import constants
+
+
+def main():
+    cluster = Cluster([constants.CASSANDRA_HOST], port=constants.CASSANDRA_PORT)
+    session = cluster.connect()
+
+    session.execute(f"""
+        CREATE KEYSPACE IF NOT EXISTS {constants.CASSANDRA_KEYSPACE}
+        WITH replication = {{'class': 'SimpleStrategy', 'replication_factor': '1'}};
+    """)
+    session.set_keyspace(constants.CASSANDRA_KEYSPACE)
+
+    session.execute(f"""
+        CREATE TABLE IF NOT EXISTS {constants.CASSANDRA_TABLE} (
+            user_id INT,
+            domain TEXT,
+            created_at DATE,
+            page_title TEXT,
+            PRIMARY KEY (user_id, page_title)
+        );
+    """)
+    print("cassandra: keyspace/table ready")
+
+    session.shutdown()
+    cluster.shutdown()
+
+if __name__ == "__main__":
+    main()
