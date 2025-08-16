e41e630eeff10a0fa9c7e7a28d3ed511295b21c8
nikitac25 <n.d.chikishev@gmail.com>
2025-08-16 16:12:32 +0200

    created ddl for clicks

---
+++ hw5/scripts/ddl/create_table_clicks.sql
@@ -0,0 +1,12 @@
+CREATE TABLE clicks (
+    click_id INT PRIMARY KEY,
+    campaign_id INT NOT NULL,
+    user_id INT NOT NULL,
+    impression_id INT NOT NULL,
+    click_timestamp DATETIME,
+    FOREIGN KEY (campaign_id) REFERENCES campaigns(campaign_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (impression_id) REFERENCES impressions(id)
+);
+
+CREATE INDEX idx_clicks_campaign_timestamp ON clicks(campaign_id, click_timestamp);
