590fa92008cef0450eabbb5922567879ad3a96fa
nikitac25 <n.d.chikishev@gmail.com>
2025-08-16 10:15:51 +0200

    hw1 v.2: updated advertisers and campaigns scripts

--- hw1/scripts/advertisers.py
+++ hw1/scripts/advertisers.py
@@ -1,17 +1,13 @@
 import sys
 import pandas as pd
 import mysql.connector
+from config import DB_CONFIG
 
 df = pd.read_csv(sys.argv[1])
 advertisers = df[['AdvertiserName']].drop_duplicates().reset_index(drop=True)
 advertisers['id'] = advertisers['AdvertiserName'].str.extract(r'_(\d+)$').astype(int)
 
-conn = mysql.connector.connect(
-    host="host.docker.internal",
-    user="root",
-    password="rootpassword",
-    database="assessment_db"
-)
+conn = mysql.connector.connect(**DB_CONFIG)
 cursor = conn.cursor()
 
 query = "INSERT INTO advertisers (id, name) VALUES (%s, %s)"
--- hw1/scripts/campaigns.py
+++ hw1/scripts/campaigns.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import mysql.connector
+from config import DB_CONFIG
 
 df = pd.read_csv(sys.argv[1])
 
@@ -8,22 +9,12 @@ df['advertiser_id'] = df['AdvertiserName'].str.extract(r'_(\d+)').astype(int)
 
 df[['TargetAgeRange', 'TargetInterest', 'TargetLocation']] = df['TargetingCriteria'].str.extract(r'(\d{2}-\d{2}),\s*(.*?),\s*(.*)')
 
-conn = mysql.connector.connect(
-    host="host.docker.internal",
-    user="root",
-    password="rootpassword",
-    database="assessment_db"
-)
+conn = mysql.connector.connect(**DB_CONFIG)
 cursor = conn.cursor()
 
-query = """
-    INSERT INTO campaigns (
-        campaign_id, advertiser_id, campaign_name,
-        campaign_start_date, campaign_end_date,
-        target_age_range, target_interest, target_location,
-        ad_slot_size, budget, remaining_budget
-    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-"""
+cursor.execute("SELECT id, name FROM locations")
+location_map = {name: loc_id for loc_id, name in cursor.fetchall()}
+df['target_location_id'] = df['TargetLocation'].map(location_map)
 
 data = df[[
     'CampaignID',
@@ -33,11 +24,20 @@ data = df[[
     'CampaignEndDate',
     'TargetAgeRange',
     'TargetInterest',
-    'TargetLocation',
+    'target_location_id',
     'AdSlotSize',
     'Budget',
     'RemainingBudget'
 ]].values.tolist()
+
+query = """
+    INSERT INTO campaigns (
+        campaign_id, advertiser_id, campaign_name,
+        campaign_start_date, campaign_end_date,
+        target_age_range, target_interest, target_location_id,
+        ad_slot_size, budget, remaining_budget
+    ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
 
 cursor.executemany(query, data)
 conn.commit()
