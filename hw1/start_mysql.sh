730d4c5f5731bcec5c5c78b814e7db9c8a379649
nikitac25 <n.d.chikishev@gmail.com>
2025-08-16 10:11:29 +0200

    hw1 v.2 updated start_mysql: - refactored db, separated locations into standalone table - impmelemnted indexes

--- hw1/start_mysql.sh
+++ hw1/start_mysql.sh
@@ -18,19 +18,24 @@ docker run -d \
   -p 3306:3306 \
   mysql:8.0
 
-sleep 20
-
+sleep 30
 
 docker exec -i mysql-db mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" <<EOF
 USE $MYSQL_DATABASE;
+
+CREATE TABLE locations (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) UNIQUE
+);
 
 CREATE TABLE users (
     user_id INT PRIMARY KEY,
     age INT,
     gender VARCHAR(10),
-    location VARCHAR(100),
+    location_id INT,
     interests TEXT,
-    signup_date DATE
+    signup_date DATE,
+    FOREIGN KEY (location_id) REFERENCES locations(id)
 );
 
 CREATE TABLE advertisers (
@@ -46,11 +51,12 @@ CREATE TABLE campaigns (
     campaign_end_date DATE,
     target_age_range VARCHAR(20),
     target_interest VARCHAR(100),
-    target_location VARCHAR(100),
+    target_location_id INT,
     ad_slot_size VARCHAR(20),
     budget DECIMAL(12, 2),
     remaining_budget DECIMAL(12, 2),
-    FOREIGN KEY (advertiser_id) REFERENCES advertisers(id)
+    FOREIGN KEY (advertiser_id) REFERENCES advertisers(id),
+    FOREIGN KEY (target_location_id) REFERENCES locations(id)
 );
 
 CREATE TABLE impressions (
@@ -58,19 +64,28 @@ CREATE TABLE impressions (
     campaign_id INT NOT NULL,
     user_id INT NOT NULL,
     device VARCHAR(50),
-    user_location VARCHAR(100),
+    location_id INT,
     timestamp DATETIME,
     bid_amount DECIMAL(10, 2),
     ad_cost DECIMAL(10, 2),
     ad_revenue DECIMAL(10, 2),
     FOREIGN KEY (campaign_id) REFERENCES campaigns(campaign_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (location_id) REFERENCES locations(id)
 );
 
 CREATE TABLE clicks (
     click_id INT PRIMARY KEY,
+    campaign_id INT NOT NULL,
+    user_id INT NOT NULL,
     impression_id INT NOT NULL,
     click_timestamp DATETIME,
+    FOREIGN KEY (campaign_id) REFERENCES campaigns(campaign_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (impression_id) REFERENCES impressions(id)
 );
+
+CREATE INDEX idx_impressions_campaign_timestamp ON impressions(campaign_id, timestamp);
+CREATE INDEX idx_clicks_campaign_timestamp ON clicks(campaign_id, click_timestamp);
+CREATE INDEX idx_campaigns_advertiser_id ON campaigns(advertiser_id);
 EOF
